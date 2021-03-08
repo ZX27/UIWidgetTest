9a0f9b61a28e955577f4a1ff5205f53132bf93ac
ZX27 <43877472+ZX27@users.noreply.github.com>
2021-03-08 16:27:22 +0800

    进度条

--- app/src/main/java/com/example/uiwidgettest/MainActivity.java
+++ app/src/main/java/com/example/uiwidgettest/MainActivity.java
@@ -6,11 +6,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText editText;
+
+    private ImageView imageView;
+
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +24,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         Button button = (Button) findViewById(R.id.button);
         editText = (EditText) findViewById(R.id.edit_text);
+        imageView = (ImageView) findViewById(R.id.image_view);
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
         button.setOnClickListener(this);
     }
 
@@ -26,8 +34,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.button:
-                String inputText = editText.getText().toString();
-                Toast.makeText(MainActivity.this, inputText, Toast.LENGTH_SHORT).show();
+                int progress=progressBar.getProgress();
+                progress=progress+9;
+                progressBar.setProgress(progress);
+//                if (progressBar.getVisibility() == View.GONE) {
+//                    progressBar.setVisibility(View.VISIBLE);
+//                } else {
+//                    progressBar.setVisibility(View.GONE);
+//                }
                 break;
             default:
                 break;
